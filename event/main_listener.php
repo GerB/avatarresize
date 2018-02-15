f8a24568f85e47198da55840a3c7ca26b6ff5d37
Ger Bruinsma <ger_bruinsma@hotmail.com>
2018-02-15 14:43:46 +0100

    Cleanup

--- event/main_listener.php
+++ event/main_listener.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  *
  * Avatar Resize. An extension for the phpBB Forum Software package.
@@ -8,7 +7,6 @@
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
-
 namespace ger\avatarresize\event;
 
 /**
@@ -28,17 +26,16 @@ class main_listener implements EventSubscriberInterface
 
     static public function getSubscribedEvents()
     {
-	return array(
-	    'core.avatar_driver_upload_move_file_before' => 'resize',
-	);
+        return array(
+            'core.avatar_driver_upload_move_file_before' => 'resize',
+        );
     }
 
     public function __construct(\phpbb\config\config $config, $phpbb_root_path, $php_ext)
     {
-	$this->config = $config;
-	$this->phpbb_root_path  = $phpbb_root_path;
-	$this->php_ext  = $php_ext;
-
+        $this->config = $config;
+        $this->phpbb_root_path = $phpbb_root_path;
+        $this->php_ext = $php_ext;
     }
 
     /**
@@ -47,57 +44,55 @@ class main_listener implements EventSubscriberInterface
      */
     public function resize($event)
     {
-	// Decide if we need to do anything at all
-	$dimension = @getimagesize($event['filedata']['filename']);
+        // Decide if we need to do anything at all
+        $dimension = @getimagesize($event['filedata']['filename']);
 
-	if ($dimension === false)
-	{
-	    return false;
-	}
+        if ($dimension === false) 
+        {
+            return false;
+        }
 
-	list($width, $height, $type) = $dimension;
+        list($width, $height, $type) = $dimension;
 
-	if (empty($width) || empty($height))
-	{
-	    return false;
-	}
-	
-	$max_width = $this->config['avatar_max_width'];
-	$max_height = $this->config['avatar_max_height'];
-	
-	if (($width > $max_width) || ($height > $max_height))
-	{
-	    // Keep proportions
-	    if ($width > $height)
-	    {
-		$new_height = round($height * ($max_width / $width));
-		$new_width = $max_width;
-	    }
-	    else
-	    {
-		$new_height = $max_height;
-		$new_width = round($width * ($max_height / $height));
-	    }
-	    
-	    $data = $this->do_resize($event['filedata'], $type, $new_width, $new_height, $width, $height);
-	}
-	else
-	{
-	    // Nothing to do
-	    return false;
-	}
-	if ($data !== false)
-	{
-	    
-	    $upload_ary = array(
-		'tmp_name' => $data['destination'],
-		'size' => $data['filesize'],
-		'name' => $event['filedata']['physical_filename'],
-		'type' => $event['filedata']['mimetype'],
-    	    );
-	    $event['file']->set_upload_ary($upload_ary);
+        if (empty($width) || empty($height)) 
+        {
+            return false;
+        }
 
-	}
+        $max_width = $this->config['avatar_max_width'];
+        $max_height = $this->config['avatar_max_height'];
+
+        if (($width > $max_width) || ($height > $max_height)) {
+            // Keep proportions
+            if ($width > $height) 
+            {
+                $new_height = round($height * ($max_width / $width));
+                $new_width = $max_width;
+            } 
+            else 
+            {
+                $new_height = $max_height;
+                $new_width = round($width * ($max_height / $height));
+            }
+
+            $data = $this->do_resize($event['filedata'], $type, $new_width, $new_height, $width, $height);
+        } 
+        else 
+        {
+            // Nothing to do
+            return false;
+        }
+        if ($data !== false) 
+        {
+
+            $upload_ary = array(
+                'tmp_name' => $data['destination'],
+                'size' => $data['filesize'],
+                'name' => $event['filedata']['physical_filename'],
+                'type' => $event['filedata']['mimetype'],
+            );
+            $event['file']->set_upload_ary($upload_ary);
+        }
     }
 
     /**
@@ -110,134 +105,133 @@ class main_listener implements EventSubscriberInterface
      * @param int $orig_height
      * @return array
      */
-    private function do_resize($filedata, $type,  $new_width, $new_height, $orig_width, $orig_height)
-    {	
-	$used_imagick = false;
-	$destination = $filedata['filename'] . '_resized';
-	// Only use ImageMagick if defined and the passthru function not disabled
-	if ($this->config['img_imagick'] && function_exists('passthru'))
-	{
-	    if (substr($this->config['img_imagick'], -1) !== '/')
-	    {
-		$this->config['img_imagick'] .= '/';
-	    }
+    private function do_resize($filedata, $type, $new_width, $new_height, $orig_width, $orig_height)
+    {
+        $used_imagick = false;
+        $destination = $filedata['filename'] . '_resized';
+        // Only use ImageMagick if defined and the passthru function not disabled
+        if ($this->config['img_imagick'] && function_exists('passthru')) 
+        {
+            if (substr($this->config['img_imagick'], -1) !== '/') 
+            {
+                $this->config['img_imagick'] .= '/';
+            }
 
-	    @passthru(escapeshellcmd($this->config['img_imagick']) . 'convert' . ((defined('PHP_OS') && preg_match('#^win#i', PHP_OS)) ? '.exe' : '') . ' -quality 85 -geometry ' . $new_width . 'x' . $new_height . ' "' . str_replace('\\', '/', $filedata['filename']) . '" "' . str_replace('\\', '/', $destination) . '"');
+            @passthru(escapeshellcmd($this->config['img_imagick']) . 'convert' . ((defined('PHP_OS') && preg_match('#^win#i', PHP_OS)) ? '.exe' : '') . ' -quality 85 -geometry ' . $new_width . 'x' . $new_height . ' "' . str_replace('\\', '/', $filedata['filename']) . '" "' . str_replace('\\', '/', $destination) . '"');
 
-	    if (file_exists($destination))
-	    {
-		$used_imagick = true;
-	    }
-	}
+            if (file_exists($destination)) 
+            {
+                $used_imagick = true;
+            }
+        }
 
-	if (!$used_imagick)
-	{
-	    if (!function_exists('get_supported_image_types'))
-	    {
-		include_once($this->phpbb_root_path . 'includes/functions_posting.' . $this->php_ext);
-	    }
-	    $type = get_supported_image_types($type);
+        if (!$used_imagick) 
+        {
+            if (!function_exists('get_supported_image_types')) 
+            {
+                include_once($this->phpbb_root_path . 'includes/functions_posting.' . $this->php_ext);
+            }
+            $type = get_supported_image_types($type);
 
-	    if ($type['gd'])
-	    {
-		// If the type is not supported, we are not able to create a thumbnail
-		if ($type['format'] === false)
-		{
-		    return false;
-		}
+            if ($type['gd']) 
+            {
+                // If the type is not supported, we are not able to create a thumbnail
+                if ($type['format'] === false) 
+                {
+                    return false;
+                }
 
-		switch ($type['format']) {
-		    case IMG_GIF:
-			$image = @imagecreatefromgif($filedata['filename']);
-			break;
+                switch ($type['format']) 
+                {
+                    case IMG_GIF:
+                        $image = @imagecreatefromgif($filedata['filename']);
+                        break;
 
-		    case IMG_JPG:
-			@ini_set('gd.jpeg_ignore_warning', 1);
-			$image = @imagecreatefromjpeg($filedata['filename']);
-			break;
+                    case IMG_JPG:
+                        @ini_set('gd.jpeg_ignore_warning', 1);
+                        $image = @imagecreatefromjpeg($filedata['filename']);
+                        break;
 
-		    case IMG_PNG:
-			$image = @imagecreatefrompng($filedata['filename']);
-			break;
+                    case IMG_PNG:
+                        $image = @imagecreatefrompng($filedata['filename']);
+                        break;
 
-		    case IMG_WBMP:
-			$image = @imagecreatefromwbmp($filedata['filename']);
-			break;
-		}
+                    case IMG_WBMP:
+                        $image = @imagecreatefromwbmp($filedata['filename']);
+                        break;
+                }
 
-		if (empty($image))
-		{
-		    return false;
-		}
+                if (empty($image)) 
+                {
+                    return false;
+                }
 
-		if ($type['version'] == 1)
-		{
-		    $new_image = imagecreate($new_width, $new_height);
+                if ($type['version'] == 1) 
+                {
+                    $new_image = imagecreate($new_width, $new_height);
 
-		    if ($new_image === false)
-		    {
-			return false;
-		    }
+                    if ($new_image === false) 
+                    {
+                        return false;
+                    }
 
-		    imagecopyresized($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $orig_width, $orig_height);
-		}
-		else
-		{
-		    $new_image = imagecreatetruecolor($new_width, $new_height);
+                    imagecopyresized($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $orig_width, $orig_height);
+                } 
+                else 
+                {
+                    $new_image = imagecreatetruecolor($new_width, $new_height);
 
-		    if ($new_image === false)
-		    {
-			return false;
-		    }
+                    if ($new_image === false) 
+                    {
+                        return false;
+                    }
 
-		    // Preserve alpha transparency (png for example)
-		    @imagealphablending($new_image, false);
-		    @imagesavealpha($new_image, true);
+                    // Preserve alpha transparency (png for example)
+                    @imagealphablending($new_image, false);
+                    @imagesavealpha($new_image, true);
+                    imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $orig_width, $orig_height);
+                }
 
-		    imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $orig_width, $orig_height);
-		}
+                // If we are in safe mode create the destination file prior to using the gd functions to circumvent a PHP bug
+                if (@ini_get('safe_mode') || @strtolower(ini_get('safe_mode')) == 'on') 
+                {
+                    @touch($destination);
+                }
 
-		// If we are in safe mode create the destination file prior to using the gd functions to circumvent a PHP bug
-		if (@ini_get('safe_mode') || @strtolower(ini_get('safe_mode')) == 'on')
-		{
-		    @touch($destination);
-		}
+                switch ($type['format']) 
+                {
+                    case IMG_GIF:
+                        imagegif($new_image, $destination);
+                        break;
 
-		switch ($type['format']) {
-		    case IMG_GIF:
-			imagegif($new_image, $destination);
-			break;
+                    case IMG_JPG:
+                        imagejpeg($new_image, $destination, 90);
+                        break;
 
-		    case IMG_JPG:
-			imagejpeg($new_image, $destination, 90);
-			break;
+                    case IMG_PNG:
+                        imagepng($new_image, $destination);
+                        break;
 
-		    case IMG_PNG:
-			imagepng($new_image, $destination);
-			break;
+                    case IMG_WBMP:
+                        imagewbmp($new_image, $destination);
+                        break;
+                }
+                imagedestroy($new_image);
+            } 
+            else 
+            {
+                return false;
+            }
+        }
 
-		    case IMG_WBMP:
-			imagewbmp($new_image, $destination);
-			break;
-		}
+        if (!file_exists($destination)) 
+        {
+            return false;
+        }
 
-		imagedestroy($new_image);
-	    }
-	    else
-	    {
-		return false;
-	    }
-	}
-
-	if (!file_exists($destination))
-	{
-	    return false;
-	}
-
-	return array(
-	    'destination' => $destination,
-	    'filesize' => filesize($destination),
-	);
+        return array(
+            'destination' => $destination,
+            'filesize' => filesize($destination),
+        );
     }
-
 }
